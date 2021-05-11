67733445e1df630ab33be474da7569e8d9772221
Jelle Bootsma <jellehb@gmail.com>
2021-05-11 18:56:57 +0200

    Fixed old macro

--- gtkruler.h
+++ gtkruler.h
@@ -47,7 +47,7 @@ extern "C" {
 #define GTK_RULER(obj)              (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_RULER, GtkRuler))
 #define GTK_RULER_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_RULER, GtkRulerClass))
 #define GTK_IS_RULER(obj)           (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_RULER))
-#define GTK_IS_RULER_CLASS(klass)   (GTK_CHECK_CLASS_TYPE ((klass), GTK_TYPE_RULER))
+#define GTK_IS_RULER_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_RULER))
 #define GTK_RULER_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_TYPE_RULER, GtkRulerClass))
 
 
